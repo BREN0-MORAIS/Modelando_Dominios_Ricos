d672e7d93b5d0252293fb63187ebbc16023bc6d2
BRENO_MORAIS <brenofranciscomorais@gmail.com>
2021-05-04 22:31:47 -0400

    initial

---
+++ PaymentContext/Modelando_Dominios_Ricos/PymentContext.Domain/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PymentContext.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ PaymentContext/Modelando_Dominios_Ricos/PymentContext.Domain/Entities/Students.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PymentContext.Domain.Entities
+{
+    public class Students
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
+    }
+}
